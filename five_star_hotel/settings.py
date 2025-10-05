f7e737873c8f796dd8e563746f8842a060503a8a
srivish12 <nalincha30@gmail.com>
2025-10-05 22:13:32 +0100

    debug=False for deployment

--- five_star_hotel/settings.py
+++ five_star_hotel/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7@ya!b7472@9s71&8*j1e)-a-b=9!qv3x+_c*alcf_*p(0tp4^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost']
 
