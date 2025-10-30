cf51fc925413f6259c74d76ebc250da184d1d904
srivish12 <nalincha30@gmail.com>
2025-10-30 14:15:41 +0000

    debug true

--- five_star_hotel/settings.py
+++ five_star_hotel/settings.py
@@ -35,7 +35,7 @@ LOGIN_REDIRECT_URL = '/reservations/my-bookings/'
 SECRET_KEY = 'django-insecure-7@ya!b7472@9s71&8*j1e)-a-b=9!qv3x+_c*alcf_*p(0tp4^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost']
 
 
